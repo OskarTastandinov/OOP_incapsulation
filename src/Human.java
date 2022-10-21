1964374b29c54f0fa31518d539a30b8f6ed5a2de
askar <tashtandinov.oskar@yandex.ru>
2022-10-21 19:46:26 +0700

    Коммит ООП

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,59 @@
 public class Human {
-    public static void dfd(String[] args){
-        System.out.println("fdf");
+    private int yearOfBirth;
+    String name;
+    private String town;
+    String job;
+
+    public Human(String name, String town, int yearOfBirth, String job) {
+        if (name != name || name == null || name == "") {
+            this.name = null;
+        }else{
+            this.name = name;
+        }
+        if (yearOfBirth < 0){
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }else {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (town != town || town == null || town == ""){
+            this.town = null;
+        }else{
+            this.town = town;
+        }
+        if (job != job || job == null || job == ""){
+            this.job = null;
+        }else{
+            this.job = job;
+        }
+    }
+
+    public String getTown() {
+        return this.town;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+
+    public void setTown(String town) {
+        if (town == null || town.isEmpty()){
+            System.out.println("Информация не указана");
+        }else{
+            this.town = town;
+        }
+
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth <= 0){
+            this.yearOfBirth = 0;
+        }else {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
+
+    public void human(){
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + getTown() +
+                ". Мне " +  getYearOfBirth() + " лет. Я работаю на должности " + job + ". Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("dfffdfd");
+        Human maxim = new Human("Максим","Минск", 35, "Бренд менеджера");
+        maxim.setTown(null);
+        maxim.human();
+
+        Human anna = new Human("Аня","Москва", 29, "методиста образовательных программ");
+        anna.human();
+
+        Human katya = new Human("Катя","Калининград", 28, "продакт-менеджера");
+        katya.human();
+
+        Human artem = new Human("Артем","Москва", 27, "директора по развитию бизнеса");
+        artem.human();
+
+        Human vladimir = new Human("Владимир","Казань", 21, "нигде не работает");
+        vladimir.human();
+
     }
 
 }
