9bfde09a9b9f5a9d33c53406a6afa27e7873f4bc
askar <tashtandinov.oskar@yandex.ru>
2022-10-25 23:59:36 +0700

    Коммит ООП2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import transport.Car;
+import transport.Car.Key;
 public class Main {
     public static void main(String[] args) {
         System.out.println("ЗАДАНИЕ 1");
@@ -7,8 +8,7 @@ public class Main {
         task2();
         System.out.println("ЗАДАНИЕ 3");
         task3();
-        System.out.println("ЗАДАНИЕ 4");
-        task4();
+
 
     }
     public static void task1(){
@@ -35,51 +35,31 @@ public class Main {
         gypsophila.flover();
     }
     public static void task3(){
+        Key keyCar = new Key("sd", "df");
+
         Car lada = new Car("Lada", "Grande",0,
                 "сборка в России", "желтого цвета", 0,
                 "Автомат", "Седан", "А042АС04",4, "Зимняя");
+        lada.setKay(keyCar);
 
+        lada.car();
         Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020,
                 "сборка в Германии", "черный цвет кузова", 3.0,
                 "Автомат", "Седан", "А042АС04",4, "Зимняя");
-
-        Car bmw = new Car("BMW", "Z8", "Сборка в Германии",
-                2021, "черный цвет кузова", 3.0, "Автомат",
-                "Седан", "А042АС04",4, "Зимняя");
-
-        Car kia = new Car("Kia", "Sportage 4 поколение", 2018, "Сборка в Южной Корее", "цвет кузова красный", 2.4,
+        audi.car();
+        Car bmw = new Car("BMW", "Z8",2021, "Сборка в Германии",
+                "черный цвет кузова",3.0,"Автомат","Седан","А042АС04",
+                4,"Зимняя");
+        bmw.car();
+        Car kia = new Car("Kia", "Sportage 4 поколение", 2018, "Сборка в Южной Корее",
+                "цвет кузова красный", 2.4,
                 "Автомат", "Седан", "А042АС04",4, "Зимняя");
+        kia.car();
+        Car hyundai = new Car("Hyundai", "Avante", 2016, "сборка в Южной Корее",
+                "цвет кузова оранжевый", 1.6, "Автомат", "Седан", "А042АС04",
+                4, "Зимняя");
+        hyundai.car();
 
-
-        Car hyundai = new Car("Hyundai", "Avante", "сборка в Южной Корее", "цвет кузова оранжевый", 1.6, 2016,
-                "Автомат", "Седан", "А042АС04",4, "Зимняя");
-
-        System.out.println(lada.getStamp() + " " + lada.getModel() + ", " + lada.getYearOfProduction() + " год выпуска, " +
-                lada.getCountryOfAssembly() + ", " + lada.getBodyColor() + ", объем двигателя - " + lada.getEngineCapacity() +
-                        ", коробка передач - " + lada.getTransmission() + ", \nтип кузова - " + lada.getBodyType() +
-                ", регистрационный номер - " + lada.getRegNumber() + ", количество мест - " + lada.getNumberOfSeats() + ", тип резины - " + lada.getTypeOfRubber());
-
-        System.out.println(audi.getStamp() + " " + audi.getModel() + ", " + audi.getYearOfProduction() + " год выпуска, " +
-                audi.getCountryOfAssembly() + ", " + audi.getBodyColor() + ", объем двигателя - " + audi.getEngineCapacity() +
-                ", коробка передач - " + audi.getTransmission() + ", \nтип кузова - " + audi.getBodyType() +
-                ", регистрационный номер - " + audi.getRegNumber() + ", количество мест - " + audi.getNumberOfSeats() + ", тип резины - " + audi.getTypeOfRubber());
-
-        System.out.println(bmw.getStamp() + " " + bmw.getModel() + ", " + bmw.getCountryOfAssembly() + ", в " +
-                bmw.getYearOfProduction() + " году, " + bmw.getBodyColor() + ", объем - " + bmw.getEngineCapacity() +
-                ", коробка передач - " + bmw.getTransmission() + ", \nтип кузова - " + bmw.getBodyType() +
-                ", регистрационный номер - " + bmw.getRegNumber() + ", количество мест - " + bmw.getNumberOfSeats() + ", тип резины - " + bmw.getTypeOfRubber());
-
-        System.out.println(kia.getStamp() + " " + kia.getModel() + ", " + kia.getYearOfProduction() + " года выпуска, " +
-                kia.getCountryOfAssembly() + ", " + kia.getBodyColor() + ", объем двигателя - " + kia.getEngineCapacity() +
-                ", коробка передач - " + kia.getTransmission() + ", \nтип кузова - " + kia.getBodyType() +
-                ", регистрационный номер - " + kia.getRegNumber() + ", количество мест - " + kia.getNumberOfSeats() + ", тип резины - " + kia.getTypeOfRubber());
-
-        System.out.println(hyundai.getStamp() + " " + hyundai.getModel() + ", " + hyundai.getCountryOfAssembly() + ", " +
-                hyundai.getBodyColor() + ", объем двигателя - " + hyundai.getEngineCapacity() + ", год выпуска - " +
-                hyundai.getYearOfProduction() + " год, коробка передач - " + hyundai.getTransmission() + ", \nтип кузова - " + hyundai.getBodyType() +
-                ", регистрационный номер - " + hyundai.getRegNumber() + ", количество мест - " + hyundai.getNumberOfSeats() + ", тип резины - " + hyundai.getTypeOfRubber());
-    }
-    public static void task4(){
 
     }
 }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -13,144 +13,7 @@ public class Car {
     private String regNumber; // регистрационный номер
     private int numberOfSeats; // количество мест
     private String typeOfRubber; // тип резины
-    public Car(String stamp, String model, int yearOfProduction,
-               String countryOfAssembly, String bodyColor, double engineCapacity,
-               String transmission, String bodyType, String regNumber, int numberOfSeats, String typeOfRubber){
-        this.transmission = transmission;
-        this.bodyType = bodyType;
-        this.regNumber = regNumber;
-        this.numberOfSeats = numberOfSeats;
-        this.typeOfRubber = typeOfRubber;
-        if(stamp == null){
-            this.stamp = "default";
-        }else{
-            this.stamp = stamp;
-        }
-        if(model == null){
-            this.model = "default";
-        }else{
-            this.model = model;
-        }
-        if(countryOfAssembly == null){
-            this.countryOfAssembly = "default";
-        }else{
-            this.countryOfAssembly = countryOfAssembly;
-        }
-        if (engineCapacity <= 0){
-            this.engineCapacity = 1.5;
-        }else{
-            this.engineCapacity = engineCapacity;
-        }
-        if (bodyColor == null){
-            this.bodyColor = "Белый";
-        }else{
-            this.bodyColor = bodyColor;
-        }
-        if (yearOfProduction <= 0){
-            this.yearOfProduction = 2000;
-        }else{
-            this.yearOfProduction = yearOfProduction;
-        }
-    }
-    public Car(String stamp, String model, String countryOfAssembly, int yearOfProduction, String bodyColor,
-               double engineCapacity, String transmission, String bodyType, String regNumber, int numberOfSeats, String typeOfRubber){
-        this.transmission = transmission;
-        this.bodyType = bodyType;
-        this.regNumber = regNumber;
-        this.numberOfSeats = numberOfSeats;
-        this.typeOfRubber = typeOfRubber;
-        if(stamp == null){
-            this.stamp = "default";
-        }else{
-            this.stamp = stamp;
-        }
-        if(model == null){
-            this.model = "default";
-        }else{
-            this.model = model;
-        }
-        if(countryOfAssembly == null){
-            this.countryOfAssembly = "default";
-        }else{
-            this.countryOfAssembly = countryOfAssembly;
-        }
-        if (engineCapacity <= 0){
-            this.engineCapacity = 1.5;
-        }else{
-            this.engineCapacity = engineCapacity;
-        }
-        if (bodyColor == null){
-            this.bodyColor = "Белый";
-        }else{
-            this.bodyColor = bodyColor;
-        }
-        if (yearOfProduction <= 0){
-            this.yearOfProduction = 2000;
-        }else{
-            this.yearOfProduction = yearOfProduction;
-        }
-    }
-    public Car(String stamp, String model, String countryOfAssembly, String bodyColor, double engineCapacity,
-               int yearOfProduction, String transmission, String bodyType, String regNumber, int numberOfSeats, String typeOfRubber){
-        this.transmission = transmission;
-        this.bodyType = bodyType;
-        this.regNumber = regNumber;
-        this.numberOfSeats = numberOfSeats;
-        this.typeOfRubber = typeOfRubber;
-        if(stamp == null){
-            this.stamp = "default";
-        }else{
-            this.stamp = stamp;
-        }
-        if(model == null){
-            this.model = "default";
-        }else{
-            this.model = model;
-        }
-        if(countryOfAssembly == null){
-            this.countryOfAssembly = "default";
-        }else{
-            this.countryOfAssembly = countryOfAssembly;
-        }
-        if (engineCapacity <= 0){
-            this.engineCapacity = 1.5;
-        }else{
-            this.engineCapacity = engineCapacity;
-        }
-        if (bodyColor == null){
-            this.bodyColor = "Белый";
-        }else{
-            this.bodyColor = bodyColor;
-        }
-        if (yearOfProduction <= 0){
-            this.yearOfProduction = 2000;
-        }else{
-            this.yearOfProduction = yearOfProduction;
-        }
-    }
-    public static void rubberСhange(){
-        int typeOfRubber = 1;
-        switch (typeOfRubber){
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 10:
-            case 11:
-            case 12:
-                System.out.println("Зимняя");
-                break;
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                System.out.println("Летняя");
-                break;
-            default:
-                System.out.println("Такого дня не существует");
-        }
-    }
+
     public String getStamp() {
         return this.stamp;
     }
@@ -189,4 +52,166 @@ public class Car {
     public String getTypeOfRubber() {
         return typeOfRubber;
     }
+    public Car(String stamp, String model, int yearOfProduction,
+               String countryOfAssembly, String bodyColor, double engineCapacity,
+               String transmission, String bodyType, String regNumber, int numberOfSeats, String typeOfRubber){
+        this.transmission = transmission;
+        this.bodyType = bodyType;
+        this.regNumber = regNumber;
+        this.numberOfSeats = numberOfSeats;
+        this.typeOfRubber = typeOfRubber;
+        if(stamp == null){
+            this.stamp = "default";
+        }else{
+            this.stamp = stamp;
+        }
+        if(model == null){
+            this.model = "default";
+        }else{
+            this.model = model;
+        }
+        if(countryOfAssembly == null){
+            this.countryOfAssembly = "default";
+        }else{
+            this.countryOfAssembly = countryOfAssembly;
+        }
+        if (engineCapacity <= 0){
+            this.engineCapacity = 1.5;
+        }else{
+            this.engineCapacity = engineCapacity;
+        }
+        if (bodyColor == null){
+            this.bodyColor = "Белый";
+        }else{
+            this.bodyColor = bodyColor;
+        }
+        if (yearOfProduction <= 0){
+            this.yearOfProduction = 2000;
+        }else{
+            this.yearOfProduction = yearOfProduction;
+        }
+
+    }
+    public void car(){
+        System.out.println(stamp + " " + model + ", " + yearOfProduction + " год выпуска, " +
+                countryOfAssembly + ", " + bodyColor + ", объем двигателя - " + engineCapacity +
+                ", коробка передач - " + transmission + ", \nтип кузова - " + bodyType +
+                ", регистрационный номер - " + regNumber + ", количество мест - " + numberOfSeats + ", тип резины - " + typeOfRubber);
+    }
+
+    public static class Key{
+        private String remoteEngineStart; // удалённый запуск двигателя
+        private String keylessAccess; // бесключевой доступ
+        private Key key;
+
+        public Key getKey() {
+            return key;
+        }
+
+        public void setKey(Key key) {
+            if (key == null){
+                this.key = key;
+            }
+        }
+
+        public Key(String remoteEngineStart, String keylessAccess){
+            if (remoteEngineStart == null || remoteEngineStart.isEmpty()){
+                this.remoteEngineStart = "default";
+            }else {
+                this.remoteEngineStart = remoteEngineStart;
+            }
+            if (keylessAccess == null || keylessAccess.isEmpty()){
+                this.keylessAccess = "default";
+            }else {
+                this.keylessAccess = keylessAccess;
+            }
+        }
+
+        public String getRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public String getKeylessAccess() {
+            return keylessAccess;
+        }
+
+        public void setKeylessAccess(String keylessAccess) {
+            this.keylessAccess = keylessAccess;
+        }
+
+        public void setRemoteEngineStart(String remoteEngineStart) {
+            this.remoteEngineStart = remoteEngineStart;
+        }
+
+
+    }
+    public static class Insurance {
+        private double InsuranceValidityPeriod; // Срок действия страховки
+        private double costOfInsurance; // Стоимость страховки
+        private int InsuranceNumber; // Номер страховки
+
+        public double getInsuranceValidityPeriod() {
+            if (InsuranceValidityPeriod <= 0){ // проверка на отрицательность и 0
+                this.InsuranceValidityPeriod = 1; // хотя бы будет единица
+            }else {
+                this.InsuranceValidityPeriod = InsuranceValidityPeriod;
+            }
+            return InsuranceValidityPeriod;
+        }
+
+        public double getCostOfInsurance() {
+            if (InsuranceValidityPeriod <= 0){ // проверка на отрицательность и 0
+                this.InsuranceValidityPeriod = 1; // хотя бы будет единица
+            }else {
+                this.InsuranceValidityPeriod = InsuranceValidityPeriod;
+            }
+            return costOfInsurance;
+        }
+
+        public int getInsuranceNumber() {
+            if (InsuranceValidityPeriod <= 0){ // проверка на отрицательность и 0
+                this.InsuranceValidityPeriod = 1; // хотя бы будет единица
+            }else {
+                this.InsuranceValidityPeriod = InsuranceValidityPeriod;
+            }
+            return InsuranceNumber;
+        }
+    }
+    public static void rubberСhange(){
+        int typeOfRubber = 1;
+        switch (typeOfRubber){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Зимняя");
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Летняя");
+                break;
+            default:
+                System.out.println("Такого дня не существует");
+        }
+    }
+
+    /*Данные параметры неизменяемые. Добавьте ко всем новым полям проверку данных, что параметры не пустые, не равны null и содержат корректные данные.
+    Добавьте метод проверки срока годности страховки - не просрочена ли она. Если страховка просрочена, то в консоль должно выводится предупреждающее
+    сообщение о том, что нужно срочно ехать оформлять новую страховку. Если страховка ещё действует, то никаких сообщений выводить не нужно.
+    Добавьте метод проверки, что номер страховки содержит 9 знаков. Если знаков больше или меньше, то вывести сообщение “Номер страховки некорректный!”
+
+    Добавьте к каждому уже созданному объекту данные о страховке, подставив собственные значения
+
+- Критерии оценки:
+            - Создан вложенный класс insurance
+    - Созданы параметры класса
+    - Создан метод проверки данных срока годности и номера страховки
+    - Все параметры вложенного класса помечены модификатором доступа private
+    - Создан конструктор для создания нового объекта класса*/
 }
